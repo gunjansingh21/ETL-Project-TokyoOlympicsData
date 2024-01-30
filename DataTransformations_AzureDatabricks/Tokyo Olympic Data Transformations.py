f74bbd7eea996c0ad34aed4c97adbf3bf382eaea
Gunjan Singh <gsgunjan07@gmail.com>
2024-01-30 16:07:57 +1100

    Update Tokyo Olympic Data Transformations.py

--- DataTransformations_AzureDatabricks/Tokyo Olympic Data Transformations.py
+++ DataTransformations_AzureDatabricks/Tokyo Olympic Data Transformations.py
@@ -2,7 +2,7 @@
 configs = {"fs.azure.account.auth.type": "OAuth",
 "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
 "fs.azure.account.oauth2.client.id": "2d8731f9-36d2-4b82-b0ac-72ca4a2e8c24",
-"fs.azure.account.oauth2.client.secret": dbutils.secre,
+"fs.azure.account.oauth2.client.secret": 'K-n8Q~vRyh-1sUBk9Bq1rwNImMhjWxqRmuKdYbk0',
 "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/b46aa8e3-1d91-428a-b230-f35159b85f42/oauth2/token"}
 
 dbutils.fs.mount(
